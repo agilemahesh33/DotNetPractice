7dfa3e7d0dd5324c10a0477c0a5087e4d091265c
Mahesh <agilemahesh33@gmail.com>
2024-06-19 15:12:06 +0530

    consuming members within a class
    Rules for consuming members within a class:
    Static Member => Static Block		//Direct Access
    Static Member => Non-Static Block		//Direct Access
    Non-Static Member => Non-Static Block	//Direct Access
    Non-Static Member => Static Block		//Can be accessed only by using the class instance
    
    Rules for consuming members out of the class:
    Static Members				//Using class name
    Non-Static Members			//Using class instance

---
+++ OOPSProject/Methods.cs
@@ -0,0 +1,37 @@
+﻿namespace OOPSProject
+{
+    /// <summary>
+    /// Rules for consuming members within a class:
+    /// Static Member => Static Block		    //Direct Access
+    /// Static Member => Non - Static Block		//Direct Access
+    /// Non-Static Member => Non - Static Block	//Direct Access
+    /// Non-Static Member => Static Block		//Can be accessed only by using the class instance
+    /// Rules for consuming members out of the class:
+    /// Static Members				//Using class name
+    /// Non-Static Members			//Using class instance
+    /// </summary>
+    internal class Methods
+    {
+        int x = 200;
+        static int y = 200;
+        public void Add()
+        {
+            Console.WriteLine(x + y);
+        }
+        public static void Sub()
+        {
+            Methods m = new Methods();
+            Console.WriteLine(m.x - y);
+        }
+    }
+    internal class TestMethod
+    {
+        static void Main()
+        {
+            Methods obj = new Methods();
+            obj.Add();
+            Methods.Sub();
+            Console.ReadLine();
+        }
+    }
+}
