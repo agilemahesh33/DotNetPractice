604dc585d0370c430df36e234600062c6fa9349c
Mahesh <agilemahesh33@gmail.com>
2024-06-19 21:03:56 +0530

    Static Constructor
    Program for demonstration of static and nostatic constructor call
    static Constructor is the first block of code that is executed before the Main()
    static Constructors use is to initialise only static and constant data members which is called only once.

---
+++ OOPSProject/Constructors.cs
@@ -0,0 +1,26 @@
+﻿namespace OOPSProject
+{
+    /// <summary>
+    /// Program for demonstration of static and nostatic constructor call
+    /// static Constructor is the first block of code that is executed before the Main()
+    /// static Constructors use is to initialise only static and constant data members which is called only once.
+    /// </summary>
+    internal class Constructors
+    {
+        //static int x = 0;
+        static Constructors()
+        {
+            Console.WriteLine("Static Constructor called");
+        }
+        Constructors()
+        {
+            Console.WriteLine("Non-Static Constructor called");
+        }
+        public static void Main()
+        {
+            Console.WriteLine("Main Method called");
+            Constructors obj = new Constructors();
+            Console.ReadLine();
+        }
+    }
+}
