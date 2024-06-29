2622ccd35de783de01f701ff8b31299444d1ad02
Mahesh <agilemahesh33@gmail.com>
2024-06-29 22:34:12 +0530

    Swap number WO Third Var
    Swap Two Numbers without Third Variable two Soulutions

---
+++ LogicalProgram/SwapNumber.cs
@@ -0,0 +1,30 @@
+﻿namespace LogicalProgram
+{
+    internal class SwapNumber
+    {
+        public static (int x, int y) SwapNoSol1(int x, int y)
+        {
+            x= x*y; 
+            y= x/y;
+            x= x/y;
+            return(x, y);
+        }
+        public static void SwapNoSol2(ref int x, ref int y)
+        {
+            x = x + y;
+            y = x - y;
+            x = x - y;            
+        }
+        public static void Main()
+        {
+            int x = 10, y = 20;
+            Console.WriteLine("Before Swap " + x + " :  " + y);
+            (x, y) = SwapNoSol1(x, y);
+            Console.WriteLine("After Swap " +  x + " :  " + y);
+            int p = 30, q = 34;
+            Console.WriteLine("Before Swap " + p + " :  " + q);
+            SwapNoSol2 (ref p ,ref q);
+            Console.WriteLine("After Swap " + p + " :  " + q);
+        }
+    }
+}
