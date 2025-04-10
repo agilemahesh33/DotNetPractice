b37de265f58f876c374ec3724032b77114b2c774
Mahesh <agilemahesh33@gmail.com>
2025-04-10 16:32:09 +0530

    Implementing Multiple interfaces
    Every class can have multiple immediate interfaces but a class can have only one Immediate Parent class

---
+++ LogicalProgram/IMath1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    internal interface IMath1
+    {
+        public void Add(double x, double y);
+        public void Sub(double x, double y);
+    }
+}
