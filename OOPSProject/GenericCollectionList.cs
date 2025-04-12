b1792b7da2d38588e7cc6400a7589a503a6c8203
Mahesh M Baradkar <agilemahesh33@gmail.com>
2025-04-12 16:23:02 +0530

    Update GenericCollectionList.cs

--- OOPSProject/GenericCollectionList.cs
+++ OOPSProject/GenericCollectionList.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -32,6 +32,40 @@ namespace OOPSProject
             {
                 Console.WriteLine(i);
             }
+            Console.WriteLine("***************************************");
+            List<int> L = new List<int>() { 10, 20, 30, 40, 50 };
+            List<string> L2 = new List<string>() { "Red", "Blue", "Green", "White", "Yellow" };
+            List<int> S = new List<int>() 
+            { 83, 47, 92, 56, 68, 11, 75, 24, 39, 88, 61, 96, 13, 59, 44, 71, 28, 99, 62, 86, 33, 58, 21, 95, 77};
+            List<int> T = new List<int>();
+            Console.WriteLine("Integer List : ");
+            foreach (int i in L)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("String List : ");
+            foreach (string str in L2)
+            {
+                Console.WriteLine(str);
+            }
+            foreach (int i in S)
+            {
+                if(i > 40) 
+                    T.Add(i);
+            }
+            Console.WriteLine("Source : ");
+            foreach (int i in S)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Target >40 : ");
+            foreach (int i in T)
+            {
+                Console.WriteLine(i);
+            }
+            T.Sort();
+            T.Reverse();
+            Console.WriteLine(String.Join(", ", T));
         }
     }
 }
