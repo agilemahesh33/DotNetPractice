d87edd048702358cb8e803e97d18f583f1fce1ce
Mahesh M Baradkar <agilemahesh33@gmail.com>
2025-04-12 11:24:36 +0530

    Generic Methods Demo

---
+++ OOPSProject/GenericMethods.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+
+public class Program
+{
+	public bool AreEqual<T>(T a,T b)
+	{
+		return a.Equals(b)?true:false;
+	}
+	public static void Main()
+	{
+		Program obj = new Program();
+		Console.WriteLine(obj.AreEqual<int>(12,23));
+		Console.WriteLine(obj.AreEqual<bool>(true,true));
+		Console.WriteLine(obj.AreEqual<string>("Mahesh","MAhesh"));
+		Console.WriteLine(obj.AreEqual<decimal>(12.23m,23.45m));
+		Console.ReadLine();
+	}
+}
